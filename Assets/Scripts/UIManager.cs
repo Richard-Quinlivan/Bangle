b77d674aa0b59e01adea3523e333a7c904f67b13
Richard Quinlivan <richardquinlivan4444@gmail.com>
2022-12-13 23:44:18 -0500

    Added starting files

---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+using TMPro;
+
+public class UIManager : MonoBehaviour
+{
+    [SerializeField]
+    private TextMeshProUGUI _bouncesRemaing;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
